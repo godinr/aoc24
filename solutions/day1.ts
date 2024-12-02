1a6a843fc934eb7e6e79c12006e7a0cb3d99ff05
Ryan <ryan.godin@live.com>
2024-12-01 22:39:13 -0500

    clean up d1

--- solutions/day1.ts
+++ solutions/day1.ts
@@ -4,12 +4,12 @@ async function readAndSortInput(fileName: string){
   const arr1: number[] = [];
   const arr2: number[] = [];
 
-  const lines = await readInput(fileName);
+  const lines = await readInput(fileName, "\r\n");
 
   for (let i = 0; i < lines.length; i++){
-    const nums = lines[i].split("   ");
-    arr1.push(Number(nums[0]));
-    arr2.push(Number(nums[1]));
+    const [n1, n2] = lines[i].split("   ");
+    arr1.push(Number(n1));
+    arr2.push(Number(n2));
   }
   
   arr1.sort((a, b) => a - b);
@@ -18,45 +18,42 @@ async function readAndSortInput(fileName: string){
   return [arr1, arr2];
 }
 
-async function p1(){
+async function part1(): Promise<number>{
   
   const [arr1, arr2] = await readAndSortInput("inputs/day1.txt");
-
   let sum = 0;
+  
   for (let i = 0; i < arr1.length; i++){
     sum += Math.abs(arr1[i] - arr2[i]);
   }
-  console.log(sum)
+  return sum;
 }
 
-async function p2(){
+async function part2(): Promise<number> {
 
   const [arr1, arr2] = await readAndSortInput("inputs/day1.txt");
-
   const map = new Map<number, number>();
-
   let sum = 0;
+
   for (let i = 0; i < arr2.length; i++){
     
-    const val = map.has(arr2[i]);
     const num = map.get(arr2[i]);
 
-    if (val && num != undefined){
+    if (num != undefined){
       map.set(arr2[i], num+1)
-    }else {
-      map.set(arr2[i], 1);
+      continue;
     }
+    map.set(arr2[i], 1);
   }
 
   for (let i = 0; i < arr1.length; i++){
-    const val = map.has(arr1[i]);
     const num = map.get(arr1[i]);
-    if (val && num != undefined){
+    if (num != undefined){
       sum += arr1[i] * num;
     }
   }
-  console.log(sum)
+  return sum;
 }
 
-p1();
-p2();
+console.log(await part1());
+console.log(await part2());
--- util/readInput.ts
+++ util/readInput.ts
@@ -1,5 +1,5 @@
-export default async function readInput(fileName: string): Promise<string[]> {
+export default async function readInput(fileName: string, sep: string): Promise<string[]> {
     const decoder = new TextDecoder("utf-8");
     const data = await Deno.readFile(fileName);
-    return decoder.decode(data).split("\r\n");
+    return decoder.decode(data).split(sep);
   }
